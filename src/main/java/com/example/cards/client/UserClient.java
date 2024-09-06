162738a8b3e3b5a9fe7d5ec8838bd4fd9d33b6b1
Shamil <71936219+shamil008@users.noreply.github.com>
2024-09-06 13:19:50 +0400

    Revert "Commit AOP"

--- src/main/java/com/example/cards/client/UserClient.java
+++ src/main/java/com/example/cards/client/UserClient.java
@@ -1,13 +1,12 @@
 package com.example.cards.client;
 
-import com.example.cards.client.decoder.CustomErrorDecoder;
 import com.example.cards.model.client.UserResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-user",url = "http://localhost:8080",configuration = CustomErrorDecoder.class)
+@FeignClient(name = "ms-user",url = "http://localhost:8080")
 public interface UserClient {
-    @GetMapping("/internal/v1/users/{id}")
+    @GetMapping("/internal/v1/users{id}")
     UserResponseDto getUser(@PathVariable Long id);
 }
--- src/main/java/com/example/cards/service/concrete/CardServiceHandler.java
+++ src/main/java/com/example/cards/service/concrete/CardServiceHandler.java
@@ -1,7 +1,5 @@
 package com.example.cards.service.concrete;
 
-import com.example.cards.aspect.LogIgnore;
-import com.example.cards.aspect.Loggable;
 import com.example.cards.client.UserClient;
 import com.example.cards.dao.repository.CardRepository;
 import com.example.cards.mapper.CardMapper;
@@ -19,7 +17,6 @@ import static com.example.cards.model.enums.CardType.DEBIT;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Loggable
 public class CardServiceHandler implements CardService {
     private final CardRepository cardRepository;
     private final UserClient userClient;
@@ -40,7 +37,7 @@ public class CardServiceHandler implements CardService {
 
     @Override
     public void createCard(CardRequest request) {
-        var user = userClient.getUser(5L);
+        var user = userClient.getUser(1L);
         log.info("User is:{}",user.getId());
         cardRepository.save(CARD_MAPPER.buildCardEntity(request));
     }
